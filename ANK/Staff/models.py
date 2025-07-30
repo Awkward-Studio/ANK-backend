02acdcdb6be3f64ad85f677c8d119270547d4ba7
Atique Khan <khanatique0000@gmail.com>
2025-07-30 07:50:43 +0530

    fix: Update __str__ method in User model to use 'name' instead of 'username'

--- ANK/Staff/models.py
+++ ANK/Staff/models.py
@@ -64,4 +64,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     def __str__(self):
-        return f"{self.username} ({self.get_role_display()})"
+        return f"{self.name} ({self.get_role_display()})"
