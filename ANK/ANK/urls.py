1819d80e22bba82694e89ee4e2a9ff7ea3ece293
Atique Khan <khanatique0000@gmail.com>
2025-08-05 09:51:46 +0530

    csrf fix

--- ANK/ANK/urls.py
+++ ANK/ANK/urls.py
@@ -26,12 +26,13 @@ from drf_spectacular.views import (
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
-
+from ANK.csrf import csrf
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/csrf/", csrf),
     path("api/", include("Events.urls")),
     path("api/", include("Guest.urls")),
     path("api/", include("Staff.urls")),
