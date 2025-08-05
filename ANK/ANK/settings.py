9221fcfcc34e69bf8113819244882835851db6cd
Atique Khan <khanatique0000@gmail.com>
2025-08-05 10:57:29 +0530

    remove session auth

--- ANK/ANK/settings.py
+++ ANK/ANK/settings.py
@@ -76,9 +76,9 @@ STATICSTORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ROOT_URLCONF = "ANK.urls"
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
-    ],
+    # "DEFAULT_AUTHENTICATION_CLASSES": [
+    #     "rest_framework.authentication.SessionAuthentication",
+    # ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
