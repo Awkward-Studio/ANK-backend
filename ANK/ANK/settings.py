bd961883d2a84ffe57e8bb11b09fd3ee947504ef
Atique Khan <khanatique0000@gmail.com>
2025-08-06 15:23:50 +0530

    removed auth

--- ANK/ANK/settings.py
+++ ANK/ANK/settings.py
@@ -96,12 +96,12 @@ if USE_JWT:
     }
 else:
     REST_FRAMEWORK = {
-        "DEFAULT_AUTHENTICATION_CLASSES": [
-            "rest_framework.authentication.SessionAuthentication",
-        ],
-        "DEFAULT_PERMISSION_CLASSES": [
-            "rest_framework.permissions.IsAuthenticated",
-        ],
+        # "DEFAULT_AUTHENTICATION_CLASSES": [
+        #     "rest_framework.authentication.SessionAuthentication",
+        # ],
+        # "DEFAULT_PERMISSION_CLASSES": [
+        #     "rest_framework.permissions.IsAuthenticated",
+        # ],
         "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     }
 
