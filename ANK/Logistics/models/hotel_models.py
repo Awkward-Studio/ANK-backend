3158d780c3239efeb9073f5dace7bf651a16f105
Atique Khan <khanatique0000@gmail.com>
2025-09-18 14:04:15 +0530

    chore: removed hotel name uniqueness

--- ANK/Logistics/models/hotel_models.py
+++ ANK/Logistics/models/hotel_models.py
@@ -10,8 +10,8 @@ class Hotel(models.Model):
     country = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
 
-    class Meta:
-        unique_together = ("name", "city")
+    # class Meta:
+    # unique_together = ("name", "city")
 
     def __str__(self):
         return f"{self.name} - {self.city}, {self.country}"
