49e5e94b9b9984503bf0a82fc85369dac1add3f7
Atique Khan <khanatique0000@gmail.com>
2025-10-03 13:53:08 +0530

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,26 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/sh
+set -e
 
-cd /app
+echo "== ls /app =="; ls -la /app
+echo "== ls /app/ANK =="; ls -la /app/ANK
 
-# ---- RELEASE PHASE ----
+# DB migrations + static
 python ANK/manage.py migrate --noinput
-python ANK/manage.py ensure_superuser || true
 python ANK/manage.py collectstatic --noinput
 
-# ---- WEB PHASE ----
-exec daphne -b 0.0.0.0 -p "${PORT:-8000}" ANK.asgi:application
+# Start ASGI
+exec daphne -b 0.0.0.0 -p 8000 ANK.asgi:application
+
+# #!/usr/bin/env bash
+# set -euo pipefail
+
+# echo "== ls /app =="; ls -la /app
+# echo "== ls /app/ANK =="; ls -la /app/ANK
+
+# # ---- RELEASE PHASE ----
+# python ANK/manage.py migrate --noinput
+# python ANK/manage.py ensure_superuser || true
+# python ANK/manage.py collectstatic --noinput
+
+# # ---- WEB PHASE ----
+# exec daphne -b 0.0.0.0 -p "${PORT:-8000}" ANK.asgi:application
