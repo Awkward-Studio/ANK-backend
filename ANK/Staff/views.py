6c27ba2a7d4b74f5f794207f4b18211c5d7a5bdf
Atique Khan <khanatique0000@gmail.com>
2025-11-26 15:59:59 +0530

    feat: Implement MessageTemplate and Staff API views, and adjust EventRegistration RSVP status choices and Guest model field nullability.

--- ANK/Staff/views.py
+++ ANK/Staff/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, serializers
+from rest_framework import status, serializers
+from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import ValidationError
 from django.conf import settings
