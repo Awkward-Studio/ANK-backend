91a807957568309d14164293abbc71016eb9a093
Omkar <omkarmishra22@gmail.com>
2025-12-16 15:37:13 +0530

    Check #5

---
+++ ANK/Events/serializers/whatsapp_message_log_serializer.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from Events.models.whatsapp_message_log import WhatsAppMessageLog
+
+
+class WhatsAppMessageLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WhatsAppMessageLog
+        fields = [
+            "id",
+            "wamid",
+            "recipient_id",
+            "status",
+            "direction",
+            "message_type",
+            "template_name",
+            "body",
+            "sent_at",
+            "delivered_at",
+            "read_at",
+            "failed_at",
+            "error_code",
+            "error_message",
+            "guest_name",
+            "event_id",
+            "event_registration_id",
+            "guest_id",
+        ]
