6860284cdd1f5dd21a56436b94c571abcdb4c9a5
omi265 <omkarmishra22@gmail.com>
2026-02-24 19:35:33 +0530

    fix(media): ensure media files are served via robust re_path

--- ANK/ANK/urls.py
+++ ANK/ANK/urls.py
@@ -11,18 +11,20 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import the include() function: from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
 from django.contrib import admin
 from django.http import HttpResponse, JsonResponse
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -107,4 +109,9 @@ urlpatterns = [
     path("api/internal/resolve-wa/<str:wa_id>/", resolve_wa, name="resolve_wa"),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve media files in all environments for now to fix the 404
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
